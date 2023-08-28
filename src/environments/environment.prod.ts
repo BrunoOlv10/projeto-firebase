c74b7ecd8ef518116df127a92f05cb360f0c7b3e
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-08-27 22:25:34 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAbjaiADSpyLgQjwZbNetq0WSyOng2SmUc",
+    authDomain: "project-firebase-e32de.firebaseapp.com",
+    projectId: "project-firebase-e32de",
+    storageBucket: "project-firebase-e32de.appspot.com",
+    messagingSenderId: "651396435710",
+    appId: "1:651396435710:web:9ea9e48a2c602735c8dfaf"
+  }
 };
